7b44121a06fd97ba2571df649894d81d7b0905a5
exersalza (Julian J.) <squidodle44@gmail.com>
2025-01-07 11:37:52 +0100

    added force read

---
+++ force_read.py
@@ -0,0 +1,22 @@
+import time
+
+def main() -> int:
+    contents = [];
+    with open("sigma_mindset.txt", "r", encoding="utf-8") as f:
+        contents = f.readlines()
+
+    for i in contents:
+        # READ READ READ
+        i = i 
+        time.sleep(.1)
+
+    print("Sigma mindset aligned")
+    # insert code here V
+
+
+
+    return 0
+
+
+if __name__ == "__main__":
+    exit(main())
